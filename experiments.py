433610a845f3063904f8dc81894e02bde1e19fbd
Matthew Turner <maturner01@gmail.com>
2020-06-16 16:30:44 -0700

    started cleanup for presentations and publication of cog sci proceedings

--- experiments.py
+++ experiments.py
@@ -171,117 +171,117 @@ def shift_experiment_trials(n_agents=25, n_iter=50, n_trials=20,
     return res
 
 
-class Myers1975:
-    '''
-    Representation of Myers 1970 experiment where feminists and
-    chauvinists gave their opinions on womens issues, then discussed
-    in groups of 4 or 5, then were polled again for their opinions.
-    This class represents the experiment in code.
+# class Myers1975:
+#     '''
+#     Representation of Myers 1970 experiment where feminists and
+#     chauvinists gave their opinions on womens issues, then discussed
+#     in groups of 4 or 5, then were polled again for their opinions.
+#     This class represents the experiment in code.
 
-    See p. 709 of Myers' 1975 paper in Human Relations for an explanation.
-    I have decided for the feminist experimental condition to use five groups
-    of five. He says N=26 in Table 3 (p. 710), but that there are five groups
-    of four or five. I don't see how there can be five groups of four or five
-    given N=26, so N=25 with five groups of five will do.
-    '''
-    def __init__(self, initial_dist=np.random.normal,
-                 initial_mean=0.75,
-                 initial_std=0.5,
-                 condition='feminist/experimental',
-                 extremity_factor=3.49,):
+#     See p. 709 of Myers' 1975 paper in Human Relations for an explanation.
+#     I have decided for the feminist experimental condition to use five groups
+#     of five. He says N=26 in Table 3 (p. 710), but that there are five groups
+#     of four or five. I don't see how there can be five groups of four or five
+#     given N=26, so N=25 with five groups of five will do.
+#     '''
+#     def __init__(self, initial_dist=np.random.normal,
+#                  initial_mean=0.75,
+#                  initial_std=0.5,
+#                  condition='feminist/experimental',
+#                  extremity_factor=3.49,):
 
-        if condition == 'feminist/experimental':
-            self.n_groups = 5
-            self.n_per_group = 5
-            self.initial_mean = 0.75
-            self.stddev = extremity_factor / 2.5
+#         if condition == 'feminist/experimental':
+#             self.n_groups = 5
+#             self.n_per_group = 5
+#             self.initial_mean = 0.75
+#             self.stddev = extremity_factor / 2.5
 
-        else:
-            raise NotImplementedError(
-                'Only experimental feminist condition implemented so far'
-            )
+#         else:
+#             raise NotImplementedError(
+#                 'Only experimental feminist condition implemented so far'
+#             )
 
-        # Initialize new group. Each will be a fully-connected network.
-        def _create_group():
+#         # Initialize new group. Each will be a fully-connected network.
+#         def _create_group():
 
-            # All agents assumed to have influence over one another.
-            graph = nx.complete_graph(self.n_per_group)
+#             # All agents assumed to have influence over one another.
+#             graph = nx.complete_graph(self.n_per_group)
 
-            # Set graph nodes to be agents with a single opinion.
-            graph = nx.relabel_nodes(
-                graph,
-                {n: Agent(n_opinions=1) for n in graph.nodes()}
-            )
+#             # Set graph nodes to be agents with a single opinion.
+#             graph = nx.relabel_nodes(
+#                 graph,
+#                 {n: Agent(n_opinions=1) for n in graph.nodes()}
+#             )
 
-            # Initialize each agent's opinions based on
-            # provided distribution.
-            for node in graph.nodes():
+#             # Initialize each agent's opinions based on
+#             # provided distribution.
+#             for node in graph.nodes():
 
-                val = np.random.normal(self.initial_mean, self.stddev)
+#                 val = np.random.normal(self.initial_mean, self.stddev)
 
-                if val >= extremity_factor:
-                    node.opinions[0] = .99
-                elif val <= -extremity_factor:
-                    node.opinions[0] = -.99
-                else:
-                    node.opinions[0] = val / extremity_factor
+#                 if val >= extremity_factor:
+#                     node.opinions[0] = .99
+#                 elif val <= -extremity_factor:
+#                     node.opinions[0] = -.99
+#                 else:
+#                     node.opinions[0] = val / extremity_factor
 
-            return Network(graph)
+#             return Network(graph)
 
-        self.groups = []
-        for group_idx in range(self.n_groups):
-            self.groups.append(_create_group())
+#         self.groups = []
+#         for group_idx in range(self.n_groups):
+#             self.groups.append(_create_group())
 
-        self.initial_opinions = [
-            node.opinions[0]
-            for group in self.groups
-            for node in group.graph.nodes()
-        ]
+#         self.initial_opinions = [
+#             node.opinions[0]
+#             for group in self.groups
+#             for node in group.graph.nodes()
+#         ]
 
-        self.extremity_factor = extremity_factor
+#         self.extremity_factor = extremity_factor
 
-    def run(self, n_iter_per_round, alpha=1.0):
+#     def run(self, n_iter_per_round, alpha=1.0):
 
-        # First, each group discusses within-group for set number of
-        # "interactions".
-        for group in self.groups:
-            for _ in range(n_iter_per_round):
-                group.iterate(alpha=alpha)
+#         # First, each group discusses within-group for set number of
+#         # "interactions".
+#         for group in self.groups:
+#             for _ in range(n_iter_per_round):
+#                 group.iterate(alpha=alpha)
 
-        # Then they form new groups where one person from
-        # each group is chosen to form a new group.
-        new_groups = []
-        for agent_idx in range(self.n_per_group):
-            new_group_agents = [
-                list(self.groups[group_idx].graph.nodes())[agent_idx]
-                for group_idx in range(self.n_groups)
-            ]
-            graph = nx.complete_graph(self.n_per_group)
-            graph = nx.relabel_nodes(
-                graph,
-                {
-                    n: new_group_agents[n_idx]
-                    for n_idx, n in enumerate(graph.nodes())
-                }
-            )
-            new_groups.append(Network(graph))
+#         # Then they form new groups where one person from
+#         # each group is chosen to form a new group.
+#         new_groups = []
+#         for agent_idx in range(self.n_per_group):
+#             new_group_agents = [
+#                 list(self.groups[group_idx].graph.nodes())[agent_idx]
+#                 for group_idx in range(self.n_groups)
+#             ]
+#             graph = nx.complete_graph(self.n_per_group)
+#             graph = nx.relabel_nodes(
+#                 graph,
+#                 {
+#                     n: new_group_agents[n_idx]
+#                     for n_idx, n in enumerate(graph.nodes())
+#                 }
+#             )
+#             new_groups.append(Network(graph))
 
-        # Interact in new groups...
-        for group in new_groups:
-            for _ in range(n_iter_per_round):
-                group.iterate(alpha=alpha)
+#         # Interact in new groups...
+#         for group in new_groups:
+#             for _ in range(n_iter_per_round):
+#                 group.iterate(alpha=alpha)
 
-        # Extract final opinions from groups.
-        self.final_opinions = [
-            node.opinions[0]
-            for group in new_groups
-            for node in group.graph.nodes()
-        ]
+#         # Extract final opinions from groups.
+#         self.final_opinions = [
+#             node.opinions[0]
+#             for group in new_groups
+#             for node in group.graph.nodes()
+#         ]
 
-        self.result = ShiftExperimentResult(
-            self.initial_opinions, self.final_opinions,
-            extremity_factor=self.extremity_factor
-        )
+#         self.result = ShiftExperimentResult(
+#             self.initial_opinions, self.final_opinions,
+#             extremity_factor=self.extremity_factor
+#         )
 
 
 class ShiftExperimentResult:
